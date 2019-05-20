ad73a5172e92b54c3a68c2043edd3f80204ed4f2
JasharBicoli <jashar36@gmail.com>
2019-05-20 15:48:11 +0200

    2

--- LoginProject/LoginService.svc.cs
+++ LoginProject/LoginService.svc.cs
@@ -10,7 +10,7 @@ using LoginProject.Interface;
 
 namespace LoginProject
 {
-    // NOTE: You can use the "Rename"  xx command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: You can use the "Rename"  xx hgh command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
     // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
     public class LoginService : Interface.ILoginService
     {
